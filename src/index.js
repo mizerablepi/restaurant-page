e1b28bd11c8806a99de93d76d039e784720bc719
miz <mizerablepi@gmail.com>
2023-07-30 20:48:15 +0530

    update index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import {header} from './header.js';
+import { header } from './header.js';
 
 let body = document.getElementsByTagName('body')[0];
-let content = document.getElementById('content');
 
-content.append(header);
+body.appendChild(header);
