718e4d59973fe5faa91b9e6e124fece7e900879b
miz <mizerablepi@gmail.com>
2023-07-31 13:07:19 +0530

    add footer

---
+++ src/footer.js
@@ -0,0 +1,3 @@
+let footer = document.createElement('footer');
+footer.innerHTML= '&copy; FryPan_ restaurant corporation 2023-24'
+export { footer };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { header } from './header.js';
 import { homeSection } from './home.js';
+import { footer } from './footer.js';
 
 let body = document.getElementsByTagName('body')[0];
 let content = document.getElementById('content');
 
 body.insertBefore(header, content);
 content.appendChild(homeSection);
+body.appendChild(footer);
